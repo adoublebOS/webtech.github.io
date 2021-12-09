0a7d6188271248569fabae70f02cc1c8c275e516
b <00011440@wiut.uz>
2021-12-09 23:04:03 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const AvatarAPI = "https://i.pravatar.cc/50"
+let avatars = document.getElementsByClassName("coach");
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomPercentage() {
+    return Math.floor(getRandomInt(30, 150));
+}
+
+
+document.getElementById("customer-count").innerHTML = getRandomInt(1000, 5000);
+document.getElementById("increase").innerHTML = randomPercentage() + "%";
